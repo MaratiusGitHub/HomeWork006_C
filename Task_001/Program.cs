d0d669af4ee090c4c806793923be28c0f703d9cf
Maratius <maratius.ru@gmail.com>
2022-08-30 18:32:07 +0300

    Task1

---
+++ Task_001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int [] GetArrayFromString(string StringArray)
+{
+    string[] nums = StringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(nums[i]);
+    }
+    return result;
+}
+
+int plus(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+        return count;
+}
+
+Console.WriteLine("Введите несколько чисел через пробел");
+int []numbers = GetArrayFromString(Console.ReadLine());
+Console.WriteLine($"Количество чисел больше нуля = {plus(numbers)}");
